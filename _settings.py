5eb07a580dd1d3ff132eae20c44edb25bb2d6f26
vsc11359 <vsc11359@node604.hydra.os>
2025-12-10 09:28:25 +0100

    Added path to Amaury's data folder in _settings.py

--- _settings.py
+++ _settings.py
@@ -54,6 +54,8 @@ flags['countrymask'] = 'shapefile'
 script_dir = os.path.abspath( os.path.dirname( __file__ ) )
 #data_dir = os.path.join(script_dir, 'data') #original from Rosa (needs data to be in my folder)
 data_dir = '/data/brussel/vo/000/bvo00012/shared_subdir/vsc10419/dem4cli/data_dem4cli/data'
+climatedata_dir = '/data/brussel/vo/000/bvo00012/vsc11137/source2suffering/data'
+
 
 # Data paths for different versions
 
